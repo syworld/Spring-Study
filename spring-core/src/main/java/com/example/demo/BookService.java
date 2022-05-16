646c1b9b0dee26d083bb812291e984489e81f291
syworld <syworldy@gmail.com>
2022-05-16 10:03:24 +0900

    ClassPathXmlApplicaitonContext

---
+++ spring-core/src/main/java/com/example/demo/BookService.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+public class BookService {
+
+  BookRepository bookRepository;
+
+  public void setBookRepository(BookRepository bookRepository){
+      this.bookRepository = bookRepository;
+  }
+
+}
--- spring-core/src/main/java/com/example/demo/DemoApplication.java
+++ spring-core/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,22 @@
 package com.example.demo;
 
+import java.util.Arrays;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-@SpringBootApplication
+
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		ApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
+		String[] beanDefinitionNames = context.getBeanDefinitionNames();
+
+		System.out.println(Arrays.toString(beanDefinitionNames));
+		BookService bookService = (BookService) context.getBean("bookService");
+		System.out.println(bookService.bookRepository != null);
+
 	}
 
 }

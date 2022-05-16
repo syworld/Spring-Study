bebcf92a5a7e417945528e5e1b243d298190e533
syworld <syworldy@gmail.com>
2022-05-16 10:43:29 +0900

    feat: AnnotationConfigApplicationContext

--- spring-core/src/main/java/com/example/demo/BookRepository.java
+++ spring-core/src/main/java/com/example/demo/BookRepository.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 import org.springframework.stereotype.Repository;
 
-@Repository
+
 public class BookRepository {
 
 }
--- spring-core/src/main/java/com/example/demo/DemoApplication.java
+++ spring-core/src/main/java/com/example/demo/DemoApplication.java
@@ -4,13 +4,16 @@ import java.util.Arrays;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
+//		ApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
+		ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
+
 		String[] beanDefinitionNames = context.getBeanDefinitionNames();
 
 		System.out.println(Arrays.toString(beanDefinitionNames));

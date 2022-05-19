ebfe2c2630e60883e637440ef67a12874e669a84
syworld <syworldy@gmail.com>
2022-05-19 20:28:49 +0900

    feat: profile 선언

--- spring-core/src/main/java/com/example/demo/AppRunner.java
+++ spring-core/src/main/java/com/example/demo/AppRunner.java
@@ -1,9 +1,11 @@
 package com.example.demo;
 
+import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -57,6 +59,12 @@ public class AppRunner implements ApplicationRunner {
     System.out.println(ctx.getBean(Single.class).getProto());
     System.out.println(ctx.getBean(Single.class).getProto());
     System.out.println(ctx.getBean(Single.class).getProto());
+
+
+    // EnvironmentCapable
+    Environment environment = ctx.getEnvironment();
+    System.out.println(Arrays.toString(environment.getActiveProfiles()));
+    System.out.println(Arrays.toString(environment.getDefaultProfiles()));
 
 
 
---
+++ spring-core/src/main/java/com/example/demo/TestConfiguration.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("test")
+public class TestConfiguration {
+  // test profile로 어플리케이션을 실행햐야만 bean을 주입받을 수 있음
+  @Bean
+  public BookRepository bookRepository(){
+    return new TestBookRepository();
+  }
+
+}
